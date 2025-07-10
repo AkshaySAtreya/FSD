96d219f4ff7a866a1fb1812edc8b52d9d6929684
Akshay S Atreya <akshayatreya.smg@gmail.com>
2025-07-10 06:38:07 +0000

    Fix Assignment_1 directory structure and add main.js, mathOperations.js

---
+++ Assignments_Java_Script/Assignment_1/mathOperations.js
@@ -0,0 +1,11 @@
+// mathOperations.js
+
+// Named export for add function
+export function add(a, b) {
+  return a + b;
+}
+
+// Named export for subtract function
+export function subtract(a, b) {
+  return a - b;
+}
